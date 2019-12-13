762628116ac7ce841b6db697766ad3ef520525a7
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-12-13 14:52:50 -0500

    Change Name and Fixing Not booting as Spring Boot

--- src/main/java/com/everis/GatewayService/ApiGatewayServiceApplication.java
+++ src/main/java/com/everis/GatewayService/ApiGatewayServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
 	}
 
 }
